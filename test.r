d7fa785b09eb4f3e749b15f8a87a2e93eb80fe7e
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-05-26 20:35:56 -0400

    added graph

--- test.r
+++ test.r
@@ -1,8 +1,9 @@
 data <- read.csv("data.csv")
 
 
+timed <- subset(data, treatment == "timed")
 
-timed <- subset(data, Treatment == "timed")
+untimed <- subset(data, treatment == "untimed")
 
-untimed <- subset(data, Treatment == "untimed")
-
+plot(untimed$et, untimed$proportion, type = "p", col = "blue",
+     xlab = "Time taken", ylab = "Proportion", main = "Plot of Column 1")
