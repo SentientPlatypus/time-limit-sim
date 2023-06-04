f7d1a9abf6dfe49a938849647d4fd83a6fb2aa08
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-06-04 17:14:19 -0400

    added sample size calculations

--- test.r
+++ test.r
@@ -1,46 +1,12 @@
 data <- read.csv("data.csv", quote="")
-
+data_no_outliers <- read.csv("data.csv", quote="")
 
 timed <- subset(data, treatment == "timed")
 
 untimed <- subset(data, treatment == "untimed")
 
+timed_no <- subset(data_no_outliers, treatment == "timed")
 
+untimed_no <- subset(data_no_outliers, treatment == "untimed")
 
-ut_timevproportion<- function() {plot(untimed$et, untimed$proportion, type = "p", col = "blue",
-     xlab = "Time taken", ylab = "Proportion", main = "Time taken vs Proportion Correct")}
-
-ut_dp_proportion<- function() {
-  stripchart(untimed["proportion"], method = "stack", offset = .5, at = 0, pch = 20,
-             col = "steelblue", main = "Timed Proportion Correct", xlab = "Proportion")
-}
-
-ut_dp_attempted <- function() {
-  stripchart(untimed["attempted"], method = "stack", offset = .5, at = 0, pch = 20,
-             col = "steelblue", main = "Timed Proportion Correct", xlab = "Proportion")
-}
-
-ut_dp_correct<- function() {
-  stripchart(untimed["correct"], method = "stack", offset = .5, at = 0, pch = 20,
-             col = "steelblue", main = "Timed Proportion Correct", xlab = "Proportion")
-}
-
-
-
-
-
-t_dp_proportion<- function() {
-  stripchart(timed["proportion"], method = "stack", offset = .5, at = 0, pch = 20,
-           col = "steelblue", main = "Timed Proportion Correct", xlab = "Proportion")
-  }
-
-t_dp_attempted <- function() {
-  stripchart(timed["attempted"], method = "stack", offset = .5, at = 0, pch = 20,
-           col = "steelblue", main = "Timed Proportion Correct", xlab = "Proportion")
-  }
-
-t_dp_correct<- function() {
-  stripchart(timed["correct"], method = "stack", offset = .5, at = 0, pch = 20,
-                col = "steelblue", main = "Timed Proportion Correct", xlab = "Proportion")
-  }
 
